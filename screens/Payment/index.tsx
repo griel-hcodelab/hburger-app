a82f78fe019d88d8113f1fac58e29609261a04ec
Pedro Henrique <phfs28@gmail.com>
2022-08-15 21:15:59 -0300

    Created the PaymentForm view

--- screens/Payment/index.tsx
+++ screens/Payment/index.tsx
@@ -1,7 +1,8 @@
 import { Dimensions, ScrollView, StatusBar } from "react-native";
 import styled from "styled-components/native";
 import Header from "../../components/Header";
-import { colors } from "../../utils/variables";
+import PayIcon from "../../components/Icons/PayIcon";
+import { colors, sizes } from "../../utils/variables";
 
 const PaymentScreen = () => {
   return (
@@ -9,6 +10,12 @@ const PaymentScreen = () => {
       <Container>
         <StatusBar barStyle="light-content" backgroundColor={colors.orange} />
         <Header title="Pagamento" />
+        <Form>
+          <FormSubmitButton>
+            <PayIcon />
+            <FormSubmitButtonText>Pagar Agora</FormSubmitButtonText>
+          </FormSubmitButton>
+        </Form>
       </Container>
     </ScrollView>
   );
@@ -18,6 +25,31 @@ const Container = styled.View`
   background-color: ${colors.dark};
   width: ${Dimensions.get("window").width}px;
   height: ${Dimensions.get("window").height}px;
+`;
+
+const Form = styled.View`
+  background-color: ${colors.white};
+  flex: 1;
+  padding: ${sizes.space * 2.6}px;
+  position: relative;
+`;
+
+const FormSubmitButton = styled.TouchableOpacity`
+  width: 100%;
+  height: 64px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.orange};
+  position: absolute;
+  bottom: 0;
+`;
+
+const FormSubmitButtonText = styled.Text`
+  color: ${colors.dark3};
+  text-transform: uppercase;
+  font-size: 16px;
+  font-weight: bold;
 `;
 
 export default PaymentScreen;
--- utils/variables.ts
+++ utils/variables.ts
@@ -1,6 +1,7 @@
 export const colors = {
   dark: "#070D0D",
   dark2: "#101D1D",
+  dark3: '#1F2525',
   gray: "#D7D7D7",
   gray2: "#EEEEEE",
   orange: "#FF760C",
