4e06e6401eb17459f15b528d57ad49c8f843935a
robertogriel <roberto@griel.com.br>
2022-08-17 20:45:42 -0300

    Burgers type

---
+++ types/CarteTypes.ts
@@ -0,0 +1,11 @@
+export type SelectedBreadTypes = {
+    id: number;
+    name: string;
+    price: number;
+}
+
+export type SelectedIngredientsTypes = {
+    id: number;
+    name: string;
+    price: number;
+}
